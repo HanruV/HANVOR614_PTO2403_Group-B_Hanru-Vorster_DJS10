5a5c7ef7811e65f39f2203cc2362553e5f6b64bd
Hanru Vorster <hanru.vorster@gmail.com>
2025-02-11 10:42:46 +0200

    fetching the data in the posts component, and storing it in the local state

--- src/assets/Posts.jsx
+++ src/assets/Posts.jsx
@@ -1,3 +1,26 @@
+import { useState, useEffect } from "react";
+
 export default function Posts() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          "https://jsonplaeholder.typicode.com/posts"
+        );
+        if (!response.ok) {
+          throw new Error("Data fetching failed");
+        }
+        const data = await response.json();
+        console.log(data);
+        setPosts(data);
+      } catch (error) {
+        console.error("Data fetching failed", error);
+      }
+    };
+    fetchData();
+  }, []);
+
   return <h1>Posts</h1>;
 }
